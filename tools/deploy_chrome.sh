f4564ccf153b19e71cdae87b1314b79a51864d6d
Simon Berger <siku2@outlook.com>
2019-01-01 04:41:09 +0100

    fixing chrome auth issue

--- tools/deploy_chrome.sh
+++ tools/deploy_chrome.sh
@@ -2,18 +2,35 @@
 
 set -e
 
+echo "[CHROME] bundling..."
+
 mkdir -p "build/chrome"
 cp -af "dist/." "build/chrome/"
 cd "build/chrome"
 echo $(cat "manifest.chrome.json") > "manifest.json"
 zip -r9 "../mas-chrome.zip" *
-
 cd ..
 
+echo "[CHROME] getting access token..."
+CHROME_AUTH=$(curl "https://www.googleapis.com/oauth2/v4/token" -d \
+"client_id=$CHROME_CLIENT_ID&client_secret=$CHROME_CLIENT_SECRET&refresh_token=$CHROME_REFRESH_TOKEN&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob")
+
+CHROME_ACCESS_TOKEN=$(echo ${CHROME_AUTH} | jq -r ".access_token")
+
+echo "[CHROME] uploading..."
 curl \
--H "Authorization: Bearer $CHROME_TOKEN"  \
+-H "Authorization: Bearer $CHROME_ACCESS_TOKEN"  \
 -H "x-goog-api-version: 2" \
 -X PUT \
 -T mas-chrome.zip \
 -v \
 https://www.googleapis.com/upload/chromewebstore/v1.1/items/$CHROME_APP_ID
+
+echo "[CHROME] publishing..."
+curl \
+-H "Authorization: Bearer $CHROME_ACCESS_TOKEN"  \
+-H "x-goog-api-version: 2" \
+-H "Content-Length: 0" \
+-X POST \
+-v \
+https://www.googleapis.com/chromewebstore/v1.1/items/$CHROME_APP_ID/publish
--- tools/deploy_firefox.sh
+++ tools/deploy_firefox.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+echo "[FIREFOX] bundling..."
 mkdir -p "build/firefox"
 cp -af "dist/." "build/firefox/"
 cd "build/firefox"
 echo $(cat "manifest.webextension.json") > "manifest.json"
 
+echo "[FIREFOX] signing..."
 npm i web-ext
-web-ext sign --api-key "${FIREFOX_API_KEY}" --api-secret "${FIREFOX_API_SECRET}" --artifacts-dir "artifact"
+npx web-ext sign --api-key "${FIREFOX_API_KEY}" --api-secret "${FIREFOX_API_SECRET}" --artifacts-dir "artifact"
 
-file="$(ls  *.xpi | tail -n1)"
+file="$(ls  artifact/*.xpi | tail -n1)"
 cp -v "$file" "../mas-firefox.xpi"
