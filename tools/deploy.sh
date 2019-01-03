df8512e16992f96c4af304f99b4e5612c00ea2b9
Simon Berger <siku2@outlook.com>
2019-01-03 15:38:37 +0100

    rip

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,12 +3,13 @@
 set -e
 
 VERSION=$(cat "dist/manifest.json" | jq -r ".version")
+RELEASE="dolos@${VERSION}"
 
-echo "Deploying version ${VERSION}"
+echo "Deploying version ${RELEASE}"
 
 tools/deploy_chrome.sh
 tools/deploy_firefox.sh
 rm -rf build
 
-npx sentry-cli releases set-commits --auto
-npx sentry-cli releases deploys "dolos@${VERSION}" new -e "production"
+npx sentry-cli releases set-commits ${RELEASE} --auto
+npx sentry-cli releases deploys ${RELEASE} new -e "production"
