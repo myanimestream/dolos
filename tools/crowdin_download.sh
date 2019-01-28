338c9e70e8178aa058a6412837f328e1792cc9b5
Simon Berger <siku2@outlook.com>
2019-01-28 10:41:16 +0100

    comments cuz why not

--- tools/crowdin_download.sh
+++ tools/crowdin_download.sh
@@ -6,10 +6,13 @@ curl \
   -F "branch=${CIRCLE_BRANCH}" \
   "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/export?key=${CROWDIN_API_KEY}"
 
+# download translation to temp file
 wget \
   -qO /tmp/crowdin-$$.zip \
   "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/download/all.zip?key=${CROWDIN_API_KEY}&branch=${CIRCLE_BRANCH}"
 
+# unzip to locales
 unzip -d dist/_locales/ /tmp/crowdin-$$.zip
 
+# delete redundant temp file
 rm /tmp/crowdin-$$.zip
