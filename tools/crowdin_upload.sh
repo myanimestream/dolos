3f01646de9093059cde92d3a1327fd0683fdb6d3
Simon Berger <siku2@outlook.com>
2019-01-28 19:06:09 +0100

    file not found my bass

--- tools/crowdin_upload.sh
+++ tools/crowdin_upload.sh
@@ -3,6 +3,6 @@
 curl \
   -F "json=true" \
   -F "branch=${CIRCLE_BRANCH}" \
-  -F "files[messages.json]=@dist/_locales/en_GB/messages.json" \
-  -F "titles[messages.json]=Extension messages" \
+  -F "files[/messages.json]=@dist/_locales/en_GB/messages.json" \
+  -F "titles[/messages.json]=Extension strings" \
   "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/update-file?key=${CROWDIN_API_KEY}"
