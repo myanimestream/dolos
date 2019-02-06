eee51ba552950d4b14eec1b275c7a41f297b9760
Simon Berger <siku2@outlook.com>
2019-02-06 18:09:26 +0100

    default to master branch for tags

--- tools/crowdin_download.sh
+++ tools/crowdin_download.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# use CIRCLE_BRANCH or master if undefined
+CROWDIN_BRANCH=${CIRCLE_BRANCH:-master}
+
 # export translations
 curl \
   -F "json=true" \
-  -F "branch=${CIRCLE_BRANCH}" \
+  -F "branch=${CROWDIN_BRANCH}" \
   "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/export?key=${CROWDIN_API_KEY}"
 
 # download translation to temp file
 wget \
   -qO /tmp/crowdin-$$.zip \
-  "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/download/all.zip?key=${CROWDIN_API_KEY}&branch=${CIRCLE_BRANCH}"
+  "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/download/all.zip?key=${CROWDIN_API_KEY}&branch=${CROWDIN_BRANCH}"
 
 # unzip to locales
 unzip -d dist/_locales/ /tmp/crowdin-$$.zip
--- tools/crowdin_upload.sh
+++ tools/crowdin_upload.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# use CIRCLE_BRANCH or master if undefined
+CROWDIN_BRANCH=${CIRCLE_BRANCH:-master}
+
 curl \
   -F "json=true" \
-  -F "branch=${CIRCLE_BRANCH}" \
+  -F "branch=${CROWDIN_BRANCH}" \
   -F "files[/messages.json]=@dist/_locales/en_GB/messages.json" \
   -F "titles[/messages.json]=Extension strings" \
   "https://api.crowdin.com/api/project/${CROWDIN_PROJECT_IDENTIFIER}/update-file?key=${CROWDIN_API_KEY}"
